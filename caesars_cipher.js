094f922aa27f77e38258cab10aadcc51c307239d
nahuelrmaury <nahuelrmaury@gmail.com>
2023-01-06 16:58:49 -0300

    Third commit

---
+++ caesars_cipher.js
@@ -0,0 +1,17 @@
+function rot13(str) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let newStr = '';
+
+    for (let i = 0; i < str.length; i++) {
+        let index = alphabet.indexOf(str[i]);
+        if (index == -1) {
+            newStr += str[i];
+        } else {
+            let newIndex = (index + 13) % 26;
+            newStr += alphabet[newIndex];
+        }
+    }
+    return newStr;
+}
+
+rot13("SERR PBQR PNZC");
